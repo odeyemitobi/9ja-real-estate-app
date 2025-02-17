785638bced54a3bd663c785b14ba65b6a16b11da
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2025-02-17 16:37:40 +0100

    Signin page made, fonts and asset added and appwrite integration for backend

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,12 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {
+    console.log("Login");
+  };
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView className="h-full mt-4" contentContainerClassName="h-full">
+        <Image
+          className="w-full h-4/6"
+          resizeMode="contain"
+          source={images.onboarding}
+        />
+        <View className="px-10">
+          <Text className="text-base uppercase font-rubik text-black-200 text-center ">
+            Welcome to CribbLink
+          </Text>
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's Connect You to {"\n"}
+            <Text className="text-primary-300">Your Dream Home</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to CribbLink with Google
+          </Text>
 
-export default SignIn
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const config ={
+    platform: "com.toby.cribblink",
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+}
