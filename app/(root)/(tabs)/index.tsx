dae022b67143853f95be4041c0031255e1e6b053
Odeyemi Tobiloba <odeyemioluwatobiloba11@gmail.com>
2025-02-17 08:50:22 +0100

    Native app setup

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -9,7 +10,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-bold text-lg my-10">Home</Text>
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/sign-in">Profile</Link>
+      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
